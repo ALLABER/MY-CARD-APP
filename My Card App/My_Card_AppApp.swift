e8021dd5f2c5a9ebd9996fd51e9117ac26b2f7e5
ALLABER <swiftallaber@gmail.com>
2022-06-13 02:10:11 +0500

    Initial Commit

---
+++ My Card App/My_Card_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_Card_AppApp.swift
+//  My Card App
+//
+//  Created by Macbook on 13.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct My_Card_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
